5f6079497d0ab2a64626fdabb97728e12f676ae5
Olga Yarosh <O_ll_y_a@mail.ru>
2023-02-12 19:49:23 +0300

    create title func and docstring for first func and second

--- str_func.py
+++ str_func.py
@@ -1,3 +1,14 @@
 def upper(text):
+    """
+    Возвращает текст со всеми заглавными буквами
+    """
     return text.upper
+
+
+def title(text_t):
+    """
+    Делает заглавными все первые буквы слов
+    """
+    return text_t.title
+
 
